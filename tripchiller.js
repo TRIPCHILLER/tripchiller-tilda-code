908295586bf7f77de57538680220a31cab00c3f0
Alexander <sasha.treyster@gmail.com>
2026-05-29 17:00:23 +0300

    Update tripchiller.js

--- tripchiller.js
+++ tripchiller.js
@@ -248,8 +248,8 @@
 
     var media = window.matchMedia ? window.matchMedia('(min-width: 980px)') : null;
     var HEADER_EYE_MOTION = {
-      maxX: 2.4,
-      maxY: 2.2,
+      maxX: 1.0,
+      maxY: 1.0,
       ease: 0.15,
       idleDelay: 1000
     };
